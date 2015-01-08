9923a9d8d24ba4c770bb3933b7b40b7e5d21863f
DQ <chfe910@gmail.com>
2015-01-09 01:02:11 +0800

    create QAlgorithm module

---
+++ src/QAlgorithm.h
@@ -0,0 +1,10 @@
+
+void FASTEST_WAY(int *a, int *t, int *e, int *x, int n)
+{
+	int *f1 = new int [n + 1];
+	int *f2 = new int [n + 1];
+	int *l1 = new int [n + 1];
+	int *l2 = new int [n + 1];
+
+	f1[1] = e[1] + a[];
+}
